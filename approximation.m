1b246bb4b89e916022b282d3159ce6def3bf472d
sveinnM <sveinmar@msn.com>
2018-03-01 16:23:10 +0000

    Update approximation.m

--- approximation.m
+++ approximation.m
@@ -1,11 +1,11 @@
-function y = approximation(T1, T, N)
+function [result] = approximation(T1, T, N)
 SWF = squareWaveFourier(T1, T, N);
 %result = zeros(length(-T/2:0.001:T/2));
 counter = 1;
 temp = 0;
 k = -N:N;
 for x = -T/2:0.001:T/2 %Ytri loopa: keyrir yfir lotuna T
-    for y = 1:(N*2+1) %Innri loopa: summar yfir öll stök í k
+    for y = 1:(N*2+1) %Innri loopa: summar yfir Ã¶ll stÃ¶k Ã­ k
         temp = temp + (SWF(y) * exp((1i)*k(y)*((2*pi)/T)*x)); %'1i' stendur fyrir imaginary
     end
     result(counter) = temp;
@@ -13,4 +13,4 @@ for x = -T/2:0.001:T/2 %Ytri loopa: keyrir yfir lotuna T
     temp = 0;
 end
 
-y = result;
+end
